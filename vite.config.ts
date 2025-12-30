37ed0bcab5f30de9b186995a84c9b7f367ed4c08
Marc Obel <mao@syseng.dk>
2025-12-30 19:44:56 +0100

    a

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/incremental-game/",
+  base: "/magic-table/",
   plugins: [
     react({
       babel: {
